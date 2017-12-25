2bdc7d20d3bb4e010722d82ba64c00a44699035a
Timothy Jensen <tim@timjensen.us>
2017-12-25 10:10:28 -0500

    Add sales page template

---
+++ templates/sales-page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Sales Page Template
+ * Template Name: NICABM Sales Page
+ * Template Post Type: nicabm_program
+ *
+ * @package     NICABM\ChildTheme
+ * @author      Tim Jensen <tim@timjensen.us>
+ * @license     GNU General Public License 2.0+
+ * @since       0.1.0
+ */
+
+namespace NICABM\ChildTheme;
+
+\genesis();
