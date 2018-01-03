b4987f1d105fc7116b928f92339686d05cac7ea9
Timothy Jensen <tim@timjensen.us>
2018-01-03 13:57:40 -0500

    Remove superfish classes from sales page menu

--- views/partials/page-nav.php
+++ views/partials/page-nav.php
@@ -6,7 +6,7 @@
 
 <nav class="nav-primary" itemscope="" itemtype="https://schema.org/SiteNavigationElement" id="genesis-nav-primary" aria-label="Main navigation">
 	<div class="wrap">
-		<ul id="menu-primary-navigation" class="menu genesis-nav-menu menu-primary js-superfish responsive-menu sf-js-enabled sf-arrows" style="touch-action: pan-y;">
+		<ul id="menu-primary-navigation" class="menu genesis-nav-menu menu-primary responsive-menu" style="touch-action: pan-y;">
 			<?php foreach ( $nav_links as $nav_link ) : ?>
 				<?php
 				unset( $classes );
