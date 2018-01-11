142c39cf9dc11454fbcbbcc5c47157134aa23609
Timothy Jensen <tim@timjensen.us>
2018-01-10 20:16:17 -0500

    Add Learn Today Use Tomorrow row

---
+++ views/acf-learn-today-use-tomorrow-row.php
@@ -0,0 +1,33 @@
+<?php
+$background_color = $row['bg_color'] ?? '#fff';
+
+?>
+
+<section class="sales-page__section sales-page__section--padding-normal learn-today-use-tomorrow" style="background-color:<?php echo esc_attr( $background_color ); ?>;">
+	<div class="wrap wrap--narrow">
+		<div class="row center-xs">
+			<h2 class="col-xs-12">Learn Today. Use Tomorrow.</h2>
+			<div class="col-xs-12 col-sm-12 row__content row__content--padding-normal">
+				<h4 style="text-align: center">NICABM’s courses are made for professionals, with a focus on real-world
+					solutions to clients’ problems.</h4>
+				<div class="row middle-xs">
+					<div class="col-xs-12 col-sm-6">
+						<h5 class="learn-today-use-tomorrow__text-block">NICABM’s courses are made for professionals, with a focus on
+							real-world solutions to clients’ problems.</h5>
+					</div>
+					<div class="col-xs-12 col-sm-6">
+						<img class="learn-today-use-tomorrow__image learn-today-use-tomorrow__image--right" src="https://s3.amazonaws.com/nicabm-stealthseminar/Porges2016-2/handsprout.jpg" alt="Plant sprout in hand">
+					</div>
+				</div>
+				<div class="row middle-xs reverse">
+					<div class="col-xs-12 col-sm-6">
+						<h5 class="learn-today-use-tomorrow__text-block">Total focus on learning and skill-building that you can implement immediately</h5>
+					</div>
+					<div class="col-xs-12 col-sm-6">
+						<img class="learn-today-use-tomorrow__image learn-today-use-tomorrow__image--left" src="https://s3.amazonaws.com/nicabm-stealthseminar/Porges2016-2/womanstudy.jpg" alt="Woman studying">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
